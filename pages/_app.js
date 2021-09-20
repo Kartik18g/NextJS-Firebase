94deb03c61048ae6cee8059d2959d95a6ac09614
Kartik18g <gupta.kartik18kg@gmail.com>
2021-09-20 11:48:51 +0530

    fiebase auth added

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import '../styles/globals.css';
+import { useContext } from 'react'
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
+import { UserContext } from '../lib/context';
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
-  console.log(pageProps)
+  const userData = useUserData()
   return (
-    <>
+    <UserContext.Provider value={userData}>
       <Toaster />
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </UserContext.Provider>
   )
 }
 
